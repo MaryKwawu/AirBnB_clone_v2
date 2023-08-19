d37dba0ed71cfcbcfe199ca314b10b842ddb2b1f
MaryKwawu <may419316@gmail.com>
2023-08-19 13:01:36 +0000

    made changes to file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,36 +4,22 @@
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
+    /airbnb-onepage/: Displays 'Hello HBNB!' from the new route
 """
 from flask import Flask
 
 app = Flask(__name__)
-
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def airbnb_onepage():
+    """Displays 'Hello HBNB!' from the new route"""
+    return "Hello HBNB! This is the /airbnb-onepage/ route."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")#!/usr/bin/python3
-"""Starts a Flask web application.
+    app.run(host="0.0.0.0", port=5000)
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
