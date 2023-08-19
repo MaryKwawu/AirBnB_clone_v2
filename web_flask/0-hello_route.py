4ac0540fe9ead14d9e2be05dbf04fe48e0ef62fc
Ubuntu <may419316@gmail.com>
2023-08-19 18:38:15 +0000

    made some changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -21,5 +21,5 @@ def airbnb_onepage():
     return "Hello HBNB! This is the /airbnb-onepage/ route."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
 
