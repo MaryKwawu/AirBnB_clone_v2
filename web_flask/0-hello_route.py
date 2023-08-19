bb635f6f5a9c7113d35c61cce15a5b709e065eda
Ubuntu <may419316@gmail.com>
2023-08-19 20:14:38 +0000

    added files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,25 +1,10 @@
-#!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-    /airbnb-onepage/: Displays 'Hello HBNB!' from the new route
-"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+@app.route("/airbnb-onepage/")
+def hello_world():
     return "Hello HBNB!"
 
-@app.route("/airbnb-onepage/", strict_slashes=False)
-def airbnb_onepage():
-    """Displays 'Hello HBNB!' from the new route"""
-    return "Hello HBNB! This is the /airbnb-onepage/ route."
-
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
-
+    app.run(host="0.0.0.0", port=5000, debug=False)
